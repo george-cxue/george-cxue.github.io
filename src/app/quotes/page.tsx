f5264121adaaea154574e62867813b22235b1571
george-cxue <georgexuechang@gmail.com>
2026-02-11 23:36:56 -0500

    init quotes

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,33 +1,36 @@
-import type { Metadata } from 'next';
-import BlockQuote from '@/components/BlockQuote';
+import type { Metadata } from "next";
+import BlockQuote from "@/components/BlockQuote";
 
 export const metadata: Metadata = {
-  title: 'Quotes - George Xue',
-  description: 'A collection of quotes I find meaningful',
+  title: "Quotes - George Xue",
+  description: "A collection of quotes I find meaningful",
 };
 
 const QUOTES = [
   {
     quote:
-      'And this I believe: that the free, exploring mind of the individual human is the most valuable thing in the world.',
-    author: 'John Steinbeck',
-    source: 'East of Eden',
+      "Always remember that to argue, and win, is to break down the reality of the person you are arguing against. It is painful to lose your reality, so be kind, even if you are right.",
+    author: "Haruki Murakami",
+  },
+  {
+    quote: "Health is a crown that the healthy wear, but only the sick can see",
+    author: "Imam Al Shafi'i",
   },
   {
     quote:
-      'Be humble for you are made of earth. Be noble for you are made of stars.',
-    author: 'Serbian proverb',
+      "If you can't explain it to a six year old, you don't understand it yourself.",
+    author: "Albert Einstein",
   },
   {
     quote:
-      'If you can\'t explain it to a six year old, you don\'t understand it yourself.',
-    author: 'Albert Einstein',
+      "Every day the clock resets. Your wins don't matter. Your failures don't matter. Don't stress on what was, fight for what could be.",
+    author: "Sean Higgins",
   },
   {
     quote:
-      'We are what we repeatedly do. Excellence, then, is not an act, but a habit.',
-    author: 'Will Durant',
-    source: 'The Story of Philosophy',
+      "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Will Durant",
+    source: "The Story of Philosophy",
   },
 ];
 
